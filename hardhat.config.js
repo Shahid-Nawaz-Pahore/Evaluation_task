0f2372aa43f8928f8889e2ed088443432cdb0fc3
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2024-09-11 09:39:47 -0700

    octatoken compile successfully

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-network-helpers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.24",
+// };
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.20",
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    artifacts: "./artifacts",
+  defaultNetwork: "sepolia",
+  sourcify: {
+    enabled: false,
   },
-  mocha: {
-    timeout: 100000,
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.SEPOLIA_API,
+      goerli: process.env.GOERLI_API,
+    },
   },
 };
