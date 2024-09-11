e602196f0e280201ae16fd7c97cf0b9a04f82ee6
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2024-09-11 03:43:09 -0700

    checkstatus , openlevel and close level functions are complete

--- contracts/octaToken.sol
+++ contracts/octaToken.sol
@@ -16,13 +16,14 @@ contract OctaToken is ERC721 {
     uint256 public constant MAX_MINTS_PER_USER = 5;  
     uint256 public constant TRANSFER_FEE = 100 * 1e16;  
     address payable public owner; 
-
+    bool public level1Open;  
+    bool public level2Open;
     uint256 public totalMinted;
     mapping(address => uint256) public userMints; 
     mapping(address => uint256[]) private _userMintedIds;  
 
     event Minted(address indexed user, uint256 quantity);
-
+    event LevelChanged(string levelState);
     modifier onlyAdmin() {
         require(msg.sender == owner, "Sorry! you are not admin"); 
         _;
@@ -55,6 +56,50 @@ contract OctaToken is ERC721 {
         payable(owner).transfer(fee);  
         emit Minted(msg.sender, _quantity);  
     }
+
+        function checkLevelStatus() internal {
+        if (!level1Open && totalMinted >= 600) {
+            level1Open = true;
+            emit LevelChanged("Level 1 opened");
+        }
+        if (!level2Open && totalMinted >= 1200) {
+            level2Open = true;
+            emit LevelChanged("Level 2 opened");
+        }
+    }
+
+    function openLevel(uint256 _level) external onlyAdmin {
+        require(_level == 1 || _level == 2, "level number is just 1 and 2");
+        if (_level == 1) {
+            require(!level1Open, "level 1 is already open");
+            require(totalMinted < 600, "cannot open Level 1 after 600 NFTs");
+            level1Open = true;
+            emit LevelChanged("level 1 opened");
+        } else if (_level == 2) {
+            require(!level2Open, "level 2 is already open");
+            require(totalMinted >= 600 && totalMinted <= 1200, "cannot open Level 2 in this range");
+            level2Open = true;
+            emit LevelChanged("level 2 opened");
+        }
+    }
+
+    function closeLevel(uint256 _level) external onlyAdmin {
+        require(_level == 1 || _level == 2, "invalid level");
+        if (_level == 1) {
+            require(level1Open, "level 1 is already closed");
+            level1Open = false;
+            emit LevelChanged("level 1 closed");
+        } else if (_level == 2) {
+            require(level2Open, "level 2 is already closed");
+            level2Open = false;
+            emit LevelChanged("level 2 closed");
+        }
+
+
+    }
+
+
+
 
 
 }
