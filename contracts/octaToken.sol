907a45dc356ed7a0bdec34f6c1b148f226f70d70
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2024-09-10 18:01:11 -0700

    ....

---
+++ contracts/octaToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+//require("@openzeppelin/contracts/token/ERC721/ERC721.sol");
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract OctaToken is ERC721 {
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds; 
+
+    uint256 public constant MAX_SUPPLY = 12000;  
+    uint256 public constant MINT_LIMIT_PER_TRANSACTION = 3;  
+    uint256 public constant MAX_MINTS_PER_USER = 5;  
+    uint256 public constant TRANSFER_FEE = 100 wei;  
+    address payable public owner; 
+
+    uint256 public totalMinted;
+    mapping(address => uint256) public userMints; 
+    mapping(address => uint256[]) private _userMintedIds;  
+
+
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        owner = payable(msg.sender);  
+    }
+
+}
+
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-network-helpers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 100000,
+  },
 };
