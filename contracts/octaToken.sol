5c1bace4e1cc94311704beb2ad96b11abfb4ca06
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2024-09-11 03:31:37 -0700

    mint fuction almost done

--- contracts/octaToken.sol
+++ contracts/octaToken.sol
@@ -14,12 +14,15 @@ contract OctaToken is ERC721 {
     uint256 public constant MAX_SUPPLY = 12000;  
     uint256 public constant MINT_LIMIT_PER_TRANSACTION = 3;  
     uint256 public constant MAX_MINTS_PER_USER = 5;  
-    uint256 public constant TRANSFER_FEE = 100 wei;  
+    uint256 public constant TRANSFER_FEE = 100 * 1e16;  
     address payable public owner; 
 
     uint256 public totalMinted;
     mapping(address => uint256) public userMints; 
     mapping(address => uint256[]) private _userMintedIds;  
+
+    event Minted(address indexed user, uint256 quantity);
+
     modifier onlyAdmin() {
         require(msg.sender == owner, "Sorry! you are not admin"); 
         _;
@@ -29,6 +32,29 @@ contract OctaToken is ERC721 {
         owner = payable(msg.sender);  
     }
     
+    function mint(uint256 _quantity) external payable {
+        require(_quantity > 0 && _quantity <= MINT_LIMIT_PER_TRANSACTION, "quantity must be between 1 and 3");
+        uint256 newTotalMinted = totalMinted + _quantity;
+        require(newTotalMinted <= MAX_SUPPLY, "max supply done");
+        require(userMints[msg.sender] + _quantity <= MAX_MINTS_PER_USER, "mint limit is already done");
+        uint256 fee = _quantity * TRANSFER_FEE;
+        require(msg.value >= fee, "insufficient feee");
+
+        for (uint i = 0; i < _quantity; i++) {
+            _tokenIds.increment();
+            uint256 tokenId;
+            unchecked {
+                tokenId = _tokenIds.current();  
+            }
+            _safeMint(msg.sender, tokenId);  
+            _userMintedIds[msg.sender].push(tokenId);  
+        }
+
+        totalMinted = newTotalMinted;  
+        userMints[msg.sender] += _quantity;   
+        payable(owner).transfer(fee);  
+        emit Minted(msg.sender, _quantity);  
+    }
 
 
 }
